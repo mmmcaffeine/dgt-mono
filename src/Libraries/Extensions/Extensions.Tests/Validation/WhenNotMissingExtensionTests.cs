937abfeeb287450894fec2fbb9748c2137578ab2
Mark Roberts <mmmcaffeine@gmail.com>
2021-01-09 22:23:50 +0000

    Added simple method to validate a string is not "missing"
    In this context I am using missing to mean null or whitespace.

---
+++ src/Libraries/Extensions/Extensions.Tests/Validation/WhenNotMissingExtensionTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Dgt.Extensions.Validation
+{
+    public static class WhenNotMissingExtensionTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("\t")]
+        [InlineData("\r\n")]
+        public static void WhenNotMissing_Given_ValueIsMissingAndParamNameIsNull_Then_ExceptionThrown(string value)
+        {
+            value.Invoking(s => s.WhenNotMissing())
+                .Should().Throw<ArgumentException>()
+                .WithMessage("Value cannot be null, whitespace, or an empty string.*")
+                .And.ParamName.Should().BeNull();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("\t")]
+        [InlineData("\r\n")]
+        public static void WhenNotMissing_Given_ValueIsMissingAndParamNameIsNotNull_Then_ExceptionThrown(string value)
+        {
+            value.Invoking(s => s.WhenNotMissing("parameter"))
+                .Should().Throw<ArgumentException>()
+                .WithMessage("Value cannot be null, whitespace, or an empty string.*")
+                .And.ParamName.Should().Be("parameter");
+        }
+
+        [Fact]
+        public static void WhenNotMissing_Given_ValueIsNotMissing_Then_Value()
+        {
+            // Arrange
+            const string expected = "This is the expected return value";
+
+            // Act
+            var actual = expected.WhenNotMissing();
+
+            // Assert
+            actual.Should().Be(expected);
+        }
+    }
+}
---
+++ src/Libraries/Extensions/Extensions/Validation/WhenNotMissingExtension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace Dgt.Extensions.Validation
+{
+    public static class WhenNotMissingExtension
+    {
+        private const string MissingStringMessage = "Value cannot be null, whitespace, or an empty string.";
+
+        // ENHANCE Figure out how to say paramName should match a parameter name as ArgumentNullException does
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [return: NotNull]
+        public static string WhenNotMissing([NotNull] this string? value, string? paramName = null)
+        {
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                return value;
+            }
+
+            if (paramName is null)
+            {
+                throw new ArgumentException(MissingStringMessage);
+            }
+            else
+            {
+                throw new ArgumentException(MissingStringMessage, paramName);
+            }
+        }
+    }
+}
