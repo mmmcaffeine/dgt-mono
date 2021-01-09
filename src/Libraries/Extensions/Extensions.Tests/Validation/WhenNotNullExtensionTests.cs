d0bdf302290ee97c63acf40b087ee623f3b868b5
Mark Roberts <mmmcaffeine@gmail.com>
2021-01-09 19:10:52 +0000

    Added simple method to validate a parameter is not null

---
+++ src/Libraries/Extensions/Extensions.Tests/Validation/WhenNotNullExtensionTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Dgt.Extensions.Validation
+{
+    public class WhenNotNullExtensionTests
+    {
+        [Fact]
+        public static void WhenNotNull_GivenValueIsNullAndParamNameIsNull_Then_ExceptionThrown()
+        {
+            ((object?) null).Invoking(x => x.WhenNotNull())
+                .Should().Throw<ArgumentNullException>()
+                .WithMessage("Value cannot be null.*")
+                .And.ParamName.Should().BeNull();
+        }
+
+        [Fact]
+        public static void WhenNotNull_GivenValueIsNullAndParamNameIsNotNull_Then_ExceptionThrown()
+        {
+            ((object?) null).Invoking(x => x.WhenNotNull("parameter"))
+                .Should().Throw<ArgumentNullException>()
+                .WithMessage("Value cannot be null.*")
+                .And.ParamName.Should().Be("parameter");
+        }
+
+        [Fact]
+        public static void WhenNotNull_Given_ValueIsNotNull_Then_Value()
+        {
+            // Arrange
+            var expected = new { };
+
+            // Act
+            var actual = expected.WhenNotNull();
+
+            // Assert
+            actual.Should().BeSameAs(expected);
+        }
+    }
+}
---
+++ src/Libraries/Extensions/Extensions/Validation/WhenNotNullExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace Dgt.Extensions.Validation
+{
+    public static class WhenNotNullExtension
+    {
+        // ENHANCE Figure out how to say paramName should match a parameter name as ArgumentNullException does
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [return: NotNull]
+        public static T WhenNotNull<T>([NotNull] this T value, string? paramName = null)
+        {
+            if (value is not null)
+            {
+                return value;
+            }
+
+            if (paramName is null)
+            {
+                throw new ArgumentNullException();
+            }
+            else
+            {
+                throw new ArgumentNullException(paramName);
+            }
+        }
+    }
+}
