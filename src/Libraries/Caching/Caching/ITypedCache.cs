a5306e5d6949d1cba5782a3e8224ac44a7a11866
Mark Roberts <mmmcaffeine@gmail.com>
2021-01-18 20:18:51 +0000

    Added ITypedCache and an implementation based on the Decorator pattern
    It is not easy for people consuming our extension method to unit test. To do so they would have to do the same as we did i.e. test against the `GetAsync` and `SetAsync` methods on `IDistributedCache`. Consumers can now inject `ITypedCache` instead of `IDistributedCache` so `GetRecordAsync` and `SetRecordAsync` can be mocked.
    
    I chose to leave the implementation as an extension method in `IDistributedCache` so if people wish to consume that interface they still have access to the method. The alternative would have been to have `ITypedCache` extend `IDistributedCache`.

---
+++ src/Libraries/Caching/Caching/ITypedCache.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+
+namespace Dgt.Caching
+{
+    public interface ITypedCache
+    {
+        [return: NotNull]
+        public Task SetRecordAsync<T>(
+            [NotNull] string key,
+            [NotNull] T value,
+            TimeSpan? absoluteExpiration = null,
+            TimeSpan? slidingExpiration = null
+        );
+
+        [return: NotNull]
+        public Task<T?> GetRecordAsync<T>([NotNull] string key);
+    }
+}
---
+++ src/Libraries/Caching/Caching/TypedCache.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Dgt.Extensions.Validation;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Dgt.Caching
+{
+    public class TypedCache : ITypedCache
+    {
+        private readonly IDistributedCache _cache;
+
+        public TypedCache(IDistributedCache cache)
+        {
+            _cache = cache.WhenNotNull(nameof(cache));
+        }
+
+        [return: NotNull]
+        public Task SetRecordAsync<T>
+        (
+            [NotNull] string key,
+            [NotNull] T value,
+            TimeSpan? absoluteExpiration = null,
+            TimeSpan? slidingExpiration = null
+        )
+        {
+            return _cache.SetRecordAsync(key, value, absoluteExpiration, slidingExpiration);
+        }
+
+        [return: NotNull]
+        public Task<T?> GetRecordAsync<T>([NotNull] string key)
+        {
+            return _cache.GetRecordAsync<T>(key);
+        }
+    }
+}
