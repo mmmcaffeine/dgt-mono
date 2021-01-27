25bd174ba3dc0bcd8dfc1acdf4835f8429c0d04f
Mark Roberts <mmmcaffeine@gmail.com>
2021-01-27 17:22:06 +0000

    Reimplemented the CreateContactCommand validation using FluentValidation
    This is an improvement over what we have previously. We now get all errors reported, rather than just the first one.

--- src/Microservices/CrmMicroservice/CrmMicroservice.WebApi/ValidateCreateContactCommandPipelineBehaviour.cs
+++ src/Microservices/CrmMicroservice/CrmMicroservice.WebApi/ValidateCreateContactCommandPipelineBehaviour.cs
@@ -1,38 +1,53 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Dgt.CrmMicroservice.Domain;
 using Dgt.Extensions.Validation;
+using FluentValidation;
 using MediatR;
 
 namespace Dgt.CrmMicroservice.WebApi
 {
-    public class ValidateCreateContactCommandPipelineBehaviour : IPipelineBehavior<CreateContactCommand, CreateContactResponse>
+    public class ValidateCreateContactCommandPipelineBehaviour
+        : AbstractValidator<CreateContactCommand>, IPipelineBehavior<CreateContactCommand, CreateContactResponse>
     {
-        private readonly IBranchRepository _branchRepository;
-
         public ValidateCreateContactCommandPipelineBehaviour(IBranchRepository branchRepository)
         {
-            _branchRepository = branchRepository.WhenNotNull();
+            _ = branchRepository.WhenNotNull();
+
+            RuleFor(x => x.Title).NotEmpty();
+            RuleFor(x => x.FirstName).NotEmpty();
+            RuleFor(x => x.LastName).NotEmpty();
+            RuleFor(x => x.BranchId).MustAsync(async (branchId, token) =>
+            {
+                try
+                {
+                    _ = await branchRepository.GetBranchAsync(branchId);
+
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }).WithMessage("Branch does not exist.");
         }
 
-        // TODO Replace with FluentValidation
-        public async Task<CreateContactResponse> Handle(CreateContactCommand request, CancellationToken cancellationToken, RequestHandlerDelegate<CreateContactResponse> next)
+        public async Task<CreateContactResponse> Handle(
+            CreateContactCommand request,
+            CancellationToken cancellationToken,
+            RequestHandlerDelegate<CreateContactResponse> next)
         {
-            if (string.IsNullOrWhiteSpace(request.Title)) throw new InvalidOperationException("Title is missing.");
-            if (string.IsNullOrWhiteSpace(request.FirstName)) throw new InvalidOperationException("FirstName is missing.");
-            if (string.IsNullOrWhiteSpace(request.LastName)) throw new InvalidOperationException("LastName is missing.");
-
-            try
+            var result = await ValidateAsync(request, cancellationToken);
+            var exceptions = result.Errors.Select(error => new InvalidOperationException(error.ErrorMessage)).ToList();
+            
+            return exceptions.Count switch
             {
-                _ = await _branchRepository.GetBranchAsync(request.BranchId);
-            }
-            catch (Exception exception)
-            {
-                throw new InvalidOperationException("Branch does not exist.", exception);
-            }
-
-            return await next();
+                0 => await next(),
+                1 => throw exceptions.Single(),
+                _ => throw new AggregateException("The request failed validation.", exceptions)
+            };
         }
     }
 }
