57cd5a9ec0116e2d391b8e8c326307c84feb2e1e
Mark Roberts <mmmcaffeine@gmail.com>
2021-01-28 20:30:55 +0000

    Extracted behaviour for converting Result into IActionResult
    By turning this code into an extension method against Result we can make it reusable from any action. This seemed better than an extension method against `ControllerBase` as you would always have to qualify that with `this`. It also seemed better than introducing our own `ControllerBase` and forcing everyone to use that.
    
    We explicitly did _not_ want this code on the `Response` class; we're intending for that to be usable from any type of application, and not tied into a HTTP context.
    
    It would have been nice if we didn't have to pass the `ControllerBase` through, but trying to construct the `ObjectResult` for a validation problem was losing information. Even though I lifted an implementation from the implementation of `ControllerBase.ValidationProblem` I was losing information by virtue of not having access to the `ProblemDetailsFactory` 😢

--- src/Microservices/CrmMicroservice/CrmMicroservice.WebApi/Controllers/ContactsController.cs
+++ src/Microservices/CrmMicroservice/CrmMicroservice.WebApi/Controllers/ContactsController.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Linq;
-using System.Net;
 using System.Threading.Tasks;
 using Dgt.CrmMicroservice.WebApi.Operations.Contacts;
 using Dgt.Extensions.Validation;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Dgt.CrmMicroservice.WebApi.Controllers
 {
@@ -26,8 +23,8 @@ namespace Dgt.CrmMicroservice.WebApi.Controllers
         {
             var query = (GetContactByIdQuery.Request) id;
             var response = await _mediator.Send(query);
-        
-            return new OkObjectResult(response.Data);
+
+            return response.CreateActionResult(this, x => new ObjectResult(x.Data));
         }
 
         [HttpPost]
@@ -35,31 +32,7 @@ namespace Dgt.CrmMicroservice.WebApi.Controllers
         {
             var response = await _mediator.Send(request);
 
-            // TODO Can I tidy this up with a switch expression when it gets moved to e.g. an action filter?
-            if (response.Successful)
-            {
-                return CreatedAtAction(nameof(Get), new {id = response.Data?.CreatedContactId}, null);
-            }
-            else if (response.Exception is not null)
-            {
-                return StatusCode((int) HttpStatusCode.InternalServerError, response.Exception.Message);
-            }
-            else if (response.ValidationResult is not null && response.ValidationResult.Errors.Any())
-            {
-                var dictionary = new ModelStateDictionary();
-
-                foreach (var error in response.ValidationResult.Errors)
-                {
-                    dictionary.AddModelError(error.PropertyName, error.ErrorMessage);
-                }
-
-                return ValidationProblem(dictionary);
-            }
-            else
-            {
-                // Not successful, but there is either no exception, or no validation errors... WTF?
-                return StatusCode((int) HttpStatusCode.InternalServerError);
-            }
+            return response.CreateActionResult(this, x => CreatedAtAction(nameof(Get), new {Id = x.Data?.CreatedContactId}, null));
         }
     }
 }
---
+++ src/Microservices/CrmMicroservice/CrmMicroservice.WebApi/ResponseExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using Dgt.Extensions.Validation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Dgt.CrmMicroservice.WebApi
+{
+    // REM We're intending to move Response into its own library, hence writing this is an extension method
+    public static class ResponseExtensions
+    {
+        private const int InternalServerError = (int) HttpStatusCode.InternalServerError;
+
+        public static IActionResult CreateActionResult<TData>(
+            this Response<TData> response,
+            ControllerBase controller,
+            Func<Response<TData>, IActionResult> onSuccess)
+        {
+            _ = response.WhenNotNull(nameof(response));
+            _ = onSuccess.WhenNotNull(nameof(response));
+
+            if (response.Successful)
+            {
+                // TODO This might throw, which should then be considered as an internal server error
+                return onSuccess(response);
+            }
+            else if (response.ValidationResult is not null && response.ValidationResult.Errors.Any())
+            {
+                var dictionary = new ModelStateDictionary();
+
+                foreach (var error in response.ValidationResult.Errors)
+                {
+                    dictionary.AddModelError(error.PropertyName, error.ErrorMessage);
+                }
+
+                return controller.ValidationProblem(dictionary);
+            }
+            else if(response.Exception is not null)
+            {
+                return controller.StatusCode(InternalServerError, response.Exception.Message);
+            }
+            else
+            {
+                // REM This is not supposed to happen! The response indicates it is not successful, but we cannot find any
+                //     validation errors, or an exception so WTF actually happened?!
+                return controller.StatusCode(InternalServerError);
+            }
+        }
+    }
+}
